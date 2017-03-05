8e77a525a044c8caaa7cc64841b123635196185c
Paul Malone <Paul Malone>
2017-03-05 16:57:59 -0500

    java copied...

--- src/main/java/com/idealfed/poc/craft/Craft.java
+++ src/main/java/com/idealfed/poc/craft/Craft.java
@@ -7,6 +7,7 @@ import org.apache.log4j.Logger;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URI;
+import java.util.Date;
 import java.util.Map;
 import java.io.InputStream;
 
@@ -28,6 +29,8 @@ import com.atlassian.sal.api.user.UserManager;
 import com.atlassian.templaterenderer.TemplateRenderer;
 import com.google.common.collect.Maps;
 import com.idealfed.jiraforms.ao.*;
+
+import net.java.ao.Query;
 
 public class Craft extends HttpServlet
 {
@@ -51,11 +54,12 @@ public class Craft extends HttpServlet
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
     	String username = userManager.getRemoteUsername(request);
-    	if (username == null || !userManager.isSystemAdmin(username))
+    	if (username == null)
     	{
     		redirectToLogin(request, response);
     		return;
-    	}
+    	}    	
+    	
     	String iwfAction = request.getParameter("ijfAction");
     	if(iwfAction==null) iwfAction="noAction";
 
@@ -67,20 +71,81 @@ public class Craft extends HttpServlet
     	if(craftFlag==null) craftFlag="";
     	String debugFlag = request.getParameter("debug");
     	if(debugFlag==null) debugFlag="";
+    	String decorator = request.getParameter("decorator");
+    	if(decorator==null) decorator="";    	
+    	String gVersionNum = request.getParameter("version");
+    	if(gVersionNum==null) gVersionNum="0";
 
+    	if ((craftFlag.equals("true")) && (!userManager.isSystemAdmin(username)))
+    	{    		
+    		templateRenderer.render("nopermission.vm", null, response.getWriter());
+    		return;
+    	}    	
+    	String outTemplate = "main.vm";
+    	if (craftFlag.equals("true"))
+    	{    	
+    		outTemplate="craft.vm";
+    	} 
+    	if (decorator.equals("true"))
+    	{    	
+    		outTemplate="main_general.vm";
+    	} 
+    	
 
     	if(iwfAction.equals("getConfig"))
     	{
     		//pull the config from AO and return...
+    		if(gVersionNum.equals("0"))
+    		{
+	    		final PrintWriter w = response.getWriter();
+	    		w.printf("{\"status\":\"OK\",\"resultSet\":[");
+	    		for (FormSet fs : ao.find(FormSet.class))
+	            {
+	                w.printf(getAoFormSetJson(fs));
+	            }
+	    		w.printf("{}]}");
+	    		w.close();
+	    		return;
+    		}
+    		else
+    		{
+    			//pull the called version
+    			int vId = new Integer(gVersionNum).intValue();
+    				//formSet must exist by ID and we need it....
+    				//OK, now get the object by ID
+    			Version v = ao.get(Version.class, vId);
+            	final PrintWriter w = response.getWriter();
+            	w.printf(v.getConfig());
+        		w.close();
+        		return;
+
+    		}
+    	}
+    	else if(iwfAction.equals("getVersions"))
+    	{
+    		//pull the config from AO and return...
     		final PrintWriter w = response.getWriter();
     		w.printf("{\"status\":\"OK\",\"resultSet\":[");
-    		for (FormSet fs : ao.find(FormSet.class))
+    		for (Version v : ao.find(Version.class))
             {
-                w.printf(getAoFormSetJson(fs));
+                w.printf("{\"id\":\""+v.getID()+"\",\"created\":\""+v.getDate()+"\",\"author\":\""+v.getAuthor()+"\"},");
             }
     		w.printf("{}]}");
     		w.close();
     		return;
+    	}
+    	else if(iwfAction.equals("getVersion"))
+    	{
+        	String versionId = request.getParameter("versionId");
+			int vId = new Integer(versionId).intValue();
+				//formSet must exist by ID and we need it....
+				//OK, now get the object by ID
+			Version v = ao.get(Version.class, vId);
+        	final PrintWriter w = response.getWriter();
+        	w.printf(v.getConfig());
+    		w.close();
+    		return;
+
     	}
     	else if(iwfAction.equals("clearConfig"))
     	{
@@ -100,8 +165,6 @@ public class Craft extends HttpServlet
     	else
     	{
         	Map<String, Object> craft = Maps.newHashMap();
-        	craft.put("testname", "this is the value of testname");
-
         	craft.put("ijfUsername",username);
         	craft.put("ijfExercise", "123");
         	craft.put("ijfVersion", "IJF Version 0.1");
@@ -110,7 +173,6 @@ public class Craft extends HttpServlet
         	craft.put("ijfDebug", debugFlag);
         	craft.put("ijfCraft", craftFlag);
         	craft.put("ijfRoot", "http://idealfedsurface:2990/jira");
-
 
         	response.setContentType("text/html;charset=utf-8");
 
@@ -171,9 +233,7 @@ public class Craft extends HttpServlet
         	craft.put("ijfScript", snippets);
         	craft.put("ijfStyle", styles);
 
-
-        	templateRenderer.render("main.vm", craft, response.getWriter());
-
+        	templateRenderer.render(outTemplate, craft, response.getWriter());
     	}
 
     }
@@ -216,6 +276,8 @@ public class Craft extends HttpServlet
 	}
 	private String getAoSnippetJson(Snippet s)
 	{
+	  try
+	  {
 		if(s.getName().equals("")) s.setName("~");
 		if(s.getSnippet().equals("")) s.setSnippet("~");
 		StringBuilder sb = new StringBuilder();
@@ -223,7 +285,27 @@ public class Craft extends HttpServlet
 		sb.append("\"name\":\"" + s.getName() + "\",");
 		sb.append("\"snippet\":\"" + s.getSnippet() + "\"},");
 		return sb.toString();
+	  }
+	  catch(Exception e)
+	  {
+		  plog.error("Failed to get snippet: " + e.getMessage());
+		  return "{}";
+	  }
 	}
+
+    private void cleanVersions(int keepNum)
+    {
+    	int ctr=0;
+		for (Version v : ao.find(Version.class, Query.select().order("id DESC")))
+        {
+			ctr++;
+			if(ctr > keepNum)
+			{
+				ao.delete(v);
+			}
+        }
+
+    }
 
 
 	@Override
@@ -232,8 +314,74 @@ public class Craft extends HttpServlet
 		String iwfAction = req.getParameter("action");
 		String inJson = req.getParameter("jsonConfig");
 		if(iwfAction==null) iwfAction="noAction";
-    	if(iwfAction.equals("saveFormConfig"))
+
+    	if(iwfAction.equals("saveFormBasic"))
     	{
+
+    		//formset exists.  And, form exists...so, use the get by ID and update the values....
+    		try
+    		{
+	    		JSONObject inForm = new JSONObject(inJson);
+	    		//JSONObject formSettings = inForm.getJSONObject("settings");
+	    		//JSONObject formFields = inForm.getJSONObject("fields");
+	    		FormSet fs = ao.get(FormSet.class, new Integer(inForm.getString("formSetId")).intValue());
+				if(fs==null) throw new JSONException("Failed to get formset");
+				//this may be a new add or a save...if Form ID == 0 it's a new add, look for that first...
+				Form f;
+				int formId = new Integer(inForm.getString("formId")).intValue();
+				if(formId==0)
+				{
+					//formSet must exist by ID and we need it....
+					//OK, now get the object by ID
+					f = ao.create(Form.class);
+		        	f.setFormSet(fs);
+				}
+				else
+				{
+					//OK, now get the object by ID
+					f = ao.get(Form.class, formId);
+					f.setFormSet(fs);
+				}
+
+	    		f.setName(inForm.getString("formName"));
+        		f.setTestIssue(inForm.getString("testIssue"));
+        		f.setFormType(inForm.getString("formType"));
+	    		f.save();
+
+	    		final PrintWriter w = res.getWriter();
+	    		w.printf("{\"status\":\"OK\"}");
+	    		w.close();
+	    		return;
+
+    		}
+    		catch(JSONException e)
+    		{
+    			//failed json read
+    			plog.error("Failed to save forms config" + e.getMessage());
+	    		final PrintWriter w = res.getWriter();
+	    		w.printf("{\"status\":\"FAIL\",message:\""+e.getMessage()+"\"}");
+	    		w.close();
+	    		return;
+    		}
+    	}
+    	else if(iwfAction.equals("saveFormConfig"))
+    	{
+
+			//backup the existing configuration first
+			StringBuffer cConfig = new StringBuffer();
+			cConfig.append("{\"status\":\"OK\",\"resultSet\":[");
+    		for (FormSet fs : ao.find(FormSet.class))
+            {
+    			cConfig.append(getAoFormSetJson(fs));
+            }
+    		cConfig.append("{}]}");
+    		Version v = ao.create(Version.class);
+    		v.setDate(new Date());
+    		v.setAuthor(userManager.getRemoteUsername(req));
+    		v.setConfig(cConfig.toString());
+    		v.save();
+    		cleanVersions(20);
+
     		//formset exists.  And, form exists...so, use the get by ID and update the values....
     		try
     		{
@@ -256,7 +404,7 @@ public class Craft extends HttpServlet
 				else
 				{
 					//OK, now get the object by ID
-					f = ao.get(Form.class, new Integer(inForm.getString("formId")).intValue());
+					f = ao.get(Form.class, formId);
 				}
 
 	    		f.setName(inForm.getString("formName"));
@@ -281,7 +429,110 @@ public class Craft extends HttpServlet
 	    		return;
     		}
     	}
-    	if(iwfAction.equals("saveSnippet"))
+    	else if(iwfAction.equals("deleteFormConfig"))
+    	{
+    		//formset exists.  And, form exists...so, use the get by ID and update the values....
+    		try
+    		{
+	    		JSONObject inForm = new JSONObject(inJson);
+				Form f;
+				int formId = new Integer(inForm.getString("formId")).intValue();
+
+					//OK, now get the object by ID
+				f = ao.get(Form.class, formId);
+				ao.delete(f);
+
+	    		final PrintWriter w = res.getWriter();
+	    		w.printf("{\"status\":\"OK\"}");
+	    		w.close();
+	    		return;
+
+    		}
+    		catch(JSONException e)
+    		{
+    			//failed json read
+    			plog.error("Failed to save forms config" + e.getMessage());
+	    		final PrintWriter w = res.getWriter();
+	    		w.printf("{\"status\":\"FAIL\",message:\""+e.getMessage()+"\"}");
+	    		w.close();
+	    		return;
+    		}
+    	}
+    	else if(iwfAction.equals("saveFormSet"))
+    	{
+    		//formset exists.  And, form exists...so, use the get by ID and update the values....
+    		try
+    		{
+	    		JSONObject inForm = new JSONObject(inJson);
+
+	    		FormSet f=null;
+				int fsId = new Integer(inForm.getString("formSetId")).intValue();
+				if(fsId==0)
+				{
+					//formSet must exist by ID and we need it....
+					//OK, now get the object by ID
+					f = ao.create(FormSet.class);
+				}
+				else
+				{
+					//OK, now get the object by ID
+					f = ao.get(FormSet.class, fsId);
+				}
+	    		f.setName(inForm.getString("name"));
+        		f.setProjectId(inForm.getString("projectId"));
+        		f.setProjectName(inForm.getString("projectName"));
+	    		f.setSettings(inForm.getString("settings"));
+	    		f.save();
+	    		final PrintWriter w = res.getWriter();
+	    		w.printf("{\"status\":\"OK\"}");
+	    		w.close();
+	    		return;
+
+    		}
+    		catch(JSONException e)
+    		{
+    			//failed json read
+    			plog.error("Failed to save form set config" + e.getMessage());
+	    		final PrintWriter w = res.getWriter();
+	    		w.printf("{\"status\":\"FAIL\",message:\""+e.getMessage()+"\"}");
+	    		w.close();
+	    		return;
+    		}
+    	}
+    	else if(iwfAction.equals("deleteFormSet"))
+    	{
+    		//formset exists.  And, form exists...so, use the get by ID and update the values....
+    		try
+    		{
+	    		JSONObject inForm = new JSONObject(inJson);
+
+	    		FormSet f=null;
+				int fsId = new Integer(inForm.getString("formSetId")).intValue();
+
+				f = ao.get(FormSet.class, fsId);
+		        for (Snippet s : f.getSnippets())
+		        {
+		        	 ao.delete(s);
+		        }
+				ao.delete(f);
+
+	    		final PrintWriter w = res.getWriter();
+	    		w.printf("{\"status\":\"OK\"}");
+	    		w.close();
+	    		return;
+
+    		}
+    		catch(JSONException e)
+    		{
+    			//failed json read
+    			plog.error("Failed to save form set config" + e.getMessage());
+	    		final PrintWriter w = res.getWriter();
+	    		w.printf("{\"status\":\"FAIL\",message:\""+e.getMessage()+"\"}");
+	    		w.close();
+	    		return;
+    		}
+    	}
+    	else if(iwfAction.equals("saveSnippet"))
     	{
     		//formset exists.  And, form exists...so, use the get by ID and update the values....
     		try
@@ -302,12 +553,41 @@ public class Craft extends HttpServlet
 				else
 				{
 					//OK, now get the object by ID
-					s = ao.get(Snippet.class, new Integer(inForm.getString("snippetId")).intValue());
+					s = ao.get(Snippet.class, snippetId);
 				}
 
 	    		s.setName(inForm.getString("name"));
         		s.setSnippet(inForm.getString("snippet"));
 	    		s.save();
+	    		final PrintWriter w = res.getWriter();
+	    		w.printf("{\"status\":\"OK\",\"result\":\""+s.getID()+"\"}");
+	    		w.close();
+	    		return;
+
+    		}
+    		catch(JSONException e)
+    		{
+    			//failed json read
+    			plog.error("Failed to save forms config" + e.getMessage());
+	    		final PrintWriter w = res.getWriter();
+	    		w.printf("{\"status\":\"FAIL\",message:\""+e.getMessage()+"\"}");
+	    		w.close();
+	    		return;
+    		}
+    	}
+    	else if(iwfAction.equals("deleteSnippet"))
+    	{
+    		//formset exists.  And, form exists...so, use the get by ID and update the values....
+    		try
+    		{
+	    		JSONObject inForm = new JSONObject(inJson);
+				Snippet s;
+				int snippetId = new Integer(inForm.getString("snippetId")).intValue();
+
+					//OK, now get the object by ID
+				s = ao.get(Snippet.class, snippetId);
+				ao.delete(s);
+
 	    		final PrintWriter w = res.getWriter();
 	    		w.printf("{\"status\":\"OK\"}");
 	    		w.close();
@@ -328,7 +608,25 @@ public class Craft extends HttpServlet
     	{
     		try
     		{
-    			//only allow to run once...
+
+    			//backup the existing configuration first
+    			StringBuffer cConfig = new StringBuffer();
+    			cConfig.append("{\"status\":\"OK\",\"resultSet\":[");
+        		for (FormSet fs : ao.find(FormSet.class))
+                {
+        			cConfig.append(getAoFormSetJson(fs));
+                }
+        		cConfig.append("{}]}");
+        		Version v = ao.create(Version.class);
+        		v.setDate(new Date());
+        		v.setAuthor(userManager.getRemoteUsername(req));
+        		v.setConfig(cConfig.toString());
+        		v.save();
+        		cleanVersions(20);
+
+        		///todo:  add clear function to remove versions > some count...
+
+    			//clear current configuration
     			boolean loaded = false;
         		for (FormSet fs : ao.find(FormSet.class))
                 {
@@ -384,7 +682,7 @@ public class Craft extends HttpServlet
 		    			Snippet s = ao.create(Snippet.class);
 		    			s.setFormSet(fs);
 		    			s.setName(jsonSnippet.getString("name"));
-		    			s.setName(jsonSnippet.getString("snippet"));
+		    			s.setSnippet(jsonSnippet.getString("snippet"));
 		        		s.save();
 		    		}
 	    		}
@@ -406,10 +704,16 @@ public class Craft extends HttpServlet
 
     		}
     	}
-		final PrintWriter w = res.getWriter();
-		w.printf("{status:\"NA\"}");
-		w.close();
-		return;
+    	else
+    	{
+
+    		final PrintWriter w = res.getWriter();
+    		w.printf("{status:\"NA\"}");
+    		w.close();
+    		return;
+
+    	}
+
 
     }
 
