b5af91d8c865f832263cf0f9fac1f6559d87bea7
Paul Malone <pmalone13@gmail.com>
2018-05-29 22:33:48 -0400

    beta 14

--- src/main/resources/js/ijf_main.js
+++ src/main/resources/js/ijf_main.js
@@ -45,7 +45,7 @@ function init(inConfigVersion)
 	/*
 	   Set g_version for this version of the JS
 	*/
-    window.g_version = "5.0.13";
+    window.g_version = "5.0.14";
 
     console.log("Initializing IJF version: " + window.g_version);
     //prevent double initializing....
